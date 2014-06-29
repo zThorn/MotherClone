0136b019a7279f705733a03bc5a1a3f871299c65
Zach Thornton <thorntonz@wit.edu>
2014-06-29 19:07:07 -0400

    Added appropriate minified game.js

--- src/game.js
+++ src/game.js
@@ -332,12 +332,7 @@ var Wallaby;
         }
         Preloader.prototype.preload = function () {
             this.game.load.image('player', 'assets/player.png');
-            this.game.load.image('grass', 'assets/grass.png');
-            this.game.load.image('dirt', 'assets/dirt.png');
-            this.game.load.image('sky', 'assets/sky.png');
-            this.game.load.image('blank', 'assets/blank.png');
             this.game.load.image('drillUpgrade', 'assets/drill_upgrade_icon.png');
-
             this.game.load.image('vendor', 'assets/vendor.png');
             this.game.load.image('gasStation', 'assets/gas_station0.png');
             this.game.load.tilemap('level', 'assets/spritesheet.json', null, Phaser.Tilemap.TILED_JSON);
